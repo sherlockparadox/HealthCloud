4f50b9136f8a6490eab50a14ee76c54a315be6c6
Kapil Shrotriya <cross399@gmail.com>
2018-04-05 15:13:06 +0530

    Registration Form
    Delete previous form and renamed the edited form

--- Front-End/Registration_Form/js/index.js
+++ Front-End/Registration_Form/js/index.js
@@ -34,7 +34,7 @@ $('.next').click(function(){
   $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     //update progress
     var step = $(e.target).data('step');
-    var percent = (parseInt(step) / 4) * 100;
+    var percent = (parseInt(step) / 5) * 100;
 
     $('.progress-bar').css({width: percent + '%'});
     //$('.progress-bar').text("Step " + step + " of 5");
